d102fb9650646fc6e28eb0871ddae88e8df8997e
Rabab <raaa555@outlook.com>
2025-02-18 20:23:16 +0300

    finshed enums exercise

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,31 @@
 fun main() {
-    println("Hello World!")
+
+    val name1= UserRole.ADMIN
+
+    val name2 = UserRole.EDITOR
+
+    val name3 = UserRole.VIEWER
+
+    checkAccess(name1)
+    checkAccess(name2)
+    checkAccess(name3)
+
+}
+
+
+enum class UserRole(var accessLevel: Int) {
+    ADMIN(1),
+    EDITOR(2),
+    VIEWER(3)
+}
+
+fun checkAccess(user:UserRole) {
+    if (user.accessLevel == 1)
+        println("sufficient access")
+
+    else if (user.accessLevel == 2)
+        println("sufficient access")
+
+    else
+        println("not sufficient access")
 }
